85ce5f5f7bf38e1ddc96549bcd852a4d30b6827e
khaled.chehab <khchehab@gmail.com>
2023-06-25 21:50:24 +0300

    Made it two ways to connect to mongo db

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,11 @@
 import { connect, disconnect } from 'mongoose';
 
 export async function connectDb(): Promise<void> {
-    await connect(`mongodb+srv://${process.env['DB_USERNAME']}:${process.env['DB_PASSWORD']}@${process.env['DB_HOST']}/${process.env['DB_NAME']}`);
+    let mongoUri = process.env['MONGODB_URI'];
+    if (!mongoUri) {
+        mongoUri = `mongodb+srv://${process.env['DB_USERNAME']}:${process.env['DB_PASSWORD']}@${process.env['DB_HOST']}/${process.env['DB_NAME']}`;
+    }
+    await connect(mongoUri);
 }
 
 export async function closeDb(): Promise<void> {
