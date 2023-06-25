c26b3ecf9c18a5f7a6b1372306606b8c2cdc4c8c
khaled.chehab <khchehab@gmail.com>
2023-06-25 20:49:23 +0300

    Removed port number for the db connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,7 @@
 import { connect, disconnect } from 'mongoose';
 
 export async function connectDb(): Promise<void> {
-    await connect(`mongodb://${process.env['DB_USERNAME']}:${process.env['DB_PASSWORD']}@${process.env['DB_HOST']}:${process.env['DB_PORT']}/${process.env['DB_NAME']}`);
+    await connect(`mongodb+srv://${process.env['DB_USERNAME']}:${process.env['DB_PASSWORD']}@${process.env['DB_HOST']}/${process.env['DB_NAME']}`);
 }
 
 export async function closeDb(): Promise<void> {
